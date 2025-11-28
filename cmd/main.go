4c4eb5c6ffedb8a44ce43a46b3a223321af72945
polonkoevv <polonkoevv.v2@gmail.com>
2025-11-28 22:59:26 +0300

    feat(core): add logger

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/polonkoevv/linkchecker/internal/config"
+	"github.com/polonkoevv/linkchecker/internal/logger"
 )
 
 func main() {
 
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
-	fmt.Println("Hello World!")
+
+	appLogger, closeLogFile, err := logger.SetupLogger(cfg.Logger.LogPath, cfg.Logger.LevelInfo)
+	if err != nil {
+		slog.Error("Error while setting up logger", slog.Any("error", err))
+		os.Exit(1)
+	}
+	slog.SetDefault(appLogger)
+	defer closeLogFile()
+
+	slog.Info("Loaded config")
 }
---
+++ internal/logger/logger.go
@@ -0,0 +1,63 @@
+package logger
+
+import (
+	"io"
+	"log/slog"
+	"os"
+	"path/filepath"
+)
+
+func SetupLogger(logFile string, logLevel string) (*slog.Logger, func() error, error) {
+	if logFile != "" {
+		logDir := filepath.Dir(logFile)
+		if err := os.MkdirAll(logDir, 0755); err != nil {
+			return nil, nil, err
+		}
+	}
+
+	var fileWriter io.Writer
+	var closeFile func() error = func() error { return nil }
+
+	if logFile != "" {
+		file, err := os.OpenFile(logFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+		if err != nil {
+			return nil, nil, err
+		}
+		fileWriter = file
+		closeFile = file.Close
+	} else {
+		fileWriter = io.Discard
+	}
+
+	var writers []io.Writer
+	if fileWriter != io.Discard {
+		writers = append(writers, fileWriter)
+	}
+	writers = append(writers, os.Stdout)
+
+	multiWriter := io.MultiWriter(writers...)
+
+	// Настраиваем уровень логирования
+	var level slog.Level
+	switch logLevel {
+	case "debug":
+		level = slog.LevelDebug
+	case "info":
+		level = slog.LevelInfo
+	case "warn":
+		level = slog.LevelWarn
+	case "error":
+		level = slog.LevelError
+	default:
+		level = slog.LevelInfo
+	}
+
+	opts := &slog.HandlerOptions{
+		Level: level,
+	}
+
+	handler := slog.NewTextHandler(multiWriter, opts)
+	logger := slog.New(handler)
+
+	return logger, closeFile, nil
+}
